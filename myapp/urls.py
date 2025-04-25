77774dbb91ee046d8b56bb51fd93c1999d573a9b
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-25 19:23:11 +0500

    Update URL routing to change home view to index and remove redundant index route

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-
+    path('', views.index, name='index'),
     path('home/', views.home, name='home'),
-    path('index/', views.index, name='index'),
     path('myjobs/', views.myjobs, name='myjobs'),
-    path('profile/', views.profile, name='profile'),]
+    path('profile/', views.profile, name='profile'),
+]
