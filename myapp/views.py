976fedebce1215be3effda82668aee0f57892d87
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-25 19:23:06 +0500

    Update home view to render home.html template instead of returning a static response

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Welcome to your Django project!</h1>')
+    return render(request, 'home.html')
 
 def index(request):
     return render(request, 'index.html')
