8607c9887dfb285ef2b548fc4d5b15c47d3e2349
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-23 12:58:13 +0500

    Update settings for production environment by disabling DEBUG and specifying ALLOWED_HOSTS for deployment

--- job_portal_shahmmer_django/settings.py
+++ job_portal_shahmmer_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lgbs%*t#7fexo#jbxfnkokkr&*x!k92t)@=nibhoa2*_0)6xar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jobportalshahmeerali.pythonanywhere.com', 'localhost', '127.0.0.1', 'www.jobportalshahmeerali.pythonanywhere.com']
 
 
 # Application definition
